c7aade2c6949e203ac0a9b9e355a35eeefaae16a
Jan Sichermann <jan@abacus.com>
2015-09-11 18:35:09 -0400

    id

--- utils/datastore.go
+++ utils/datastore.go
@@ -3,6 +3,11 @@ package utils
 import (
 	"appengine"
 	"appengine/datastore"
+	"crypto/md5"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 type DatastoreObject interface {
@@ -10,8 +15,13 @@ type DatastoreObject interface {
 	EntityType() string
 }
 
-func keyInContextForObject(ctx appengine.Context, obj DatastoreObject) datastore.Key {
-	return datastore.NewKey(ctx, obj.EntityType(), obj.ObjectId(), 0, nil)
+type SettableId interface {
+	SetObjectId(i string)
+}
+
+func keyInContextForObject(ctx appengine.Context, obj DatastoreObject) *datastore.Key {
+	k := datastore.NewKey(ctx, obj.EntityType(), obj.ObjectId(), 0, nil)
+	return k
 }
 
 func PutObject(ctx appengine.Context, obj DatastoreObject) error {
@@ -22,7 +32,6 @@ func PutObject(ctx appengine.Context, obj DatastoreObject) error {
 }
 
 func GetObject(ctx appengine.Context, obj DatastoreObject) error {
-	key := datastore.NewKey(ctx, obj.EntityType(), obj.ObjectId(), 0, nil)
 	if err := datastore.Get(ctx, keyInContextForObject(ctx, obj), obj); err != nil {
 		return err
 	}
@@ -30,6 +39,17 @@ func GetObject(ctx appengine.Context, obj DatastoreObject) error {
 }
 
 func DeleteObject(ctx appengine.Context, obj DatastoreObject) error {
-	key := datastore.NewKey(ctx, obj.EntityType(), obj.ObjectId(), 0, nil)
 	return datastore.Delete(ctx, keyInContextForObject(ctx, obj))
+}
+
+/*
+NewID ...
+*/
+func NewID() string {
+	tb := make([]byte, 64)
+	tc := binary.PutUvarint(tb, uint64(time.Now().UnixNano()))
+	rb := make([]byte, 64)
+	rc := binary.PutUvarint(rb, uint64(rand.Int63()))
+	b := append(tb[:tc], rb[:rc]...)
+	return fmt.Sprintf("%x", md5.Sum(b))
 }
