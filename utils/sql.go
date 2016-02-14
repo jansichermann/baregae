4d993628fb3f5f33e8235018ee6b9d61033270b9
Jan Sichermann <jansichermann@gmail.com>
2016-02-14 17:48:51 -0500

    preliminary cloudsql stuff

---
+++ utils/sql.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"reflect"
+)
+
+type SqlObject interface {
+	InsertParameters() map[string]interface{}
+	TableName() string
+}
+
+func CloudSqlDatabase(user, password, projectId, cloudsqlInstance, database string) (*sql.DB, error) {
+	return sql.Open("mysql", user+":"+password+"@cloudsql("+projectId+":"+cloudsqlInstance+")/"+database)
+}
+
+func InsertObject(db *sql.DB, obj SqlObject) error {
+	insertParameters := obj.InsertParameters()
+	parameterNames := make([]string, len(insertParameters))
+	parameterValues := make([]interface{}, len(insertParameters))
+	placeholders := make([]string, len(insertParameters))
+	for k, v := range insertParameters {
+		parameterNames = append(parameterNames, k)
+		parameterValues = append(parameterValues, v)
+		placeholders = append(placeholders, "?")
+	}
+	keyString := strings.Join(parameterNames, ", ")
+	placeholderString := strings.Join(placeholders, ", ")
+	queryString := "INSERT INTO " + obj.TableName() + " (" + keyString + ") VALUES (" + placeholderString + ")"
+	_, err := db.Exec(queryString, parameterValues...)
+	return err
+}
